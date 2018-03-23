f2cbc08bbf317d70f6d08d2ddf072a790bb24d34
Ignacio Martinez <gsikmo@gmail.com>
2018-03-23 11:14:20 -0600

    cambios en global

--- config/global.php
+++ config/global.php
@@ -3,7 +3,7 @@
 /*IP de la pc servidor de base de datps*/
 define("DB_HOST", "localhost");
 
-define("DB_NAME", "mrburguer");
+define("DB_NAME", "base2");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
 define("DB_ENCODE", "utf8");
