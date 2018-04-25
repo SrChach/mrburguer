b40ca3a38facdee7dafd8093e8ad68da93ba5da9
Ignacio Martinez <gsikmo@gmail.com>
2018-04-25 14:38:03 -0500

    añadido limite a consulta

--- modelos/Venta.php
+++ modelos/Venta.php
@@ -51,7 +51,7 @@ Class Venta{
 	}
 
 	public function listar($idEmpleado){
-		$sql = "SELECT V.idVenta, concat(E.nomPila, ' ', E.apPaterno, ' ', E.apMaterno) as 'nombreEmpleado', V.fecha, V.montoTotal, V.descuentoActual, V.status, V.pagoTarjeta FROM venta V join empleado E on E.idEmpleado=V.idEmpleado WHERE V.idEmpleado='$idEmpleado' and ( DATE_FORMAT(current_timestamp, '%m %d %Y') = DATE_FORMAT(V.fecha, '%m %d %Y')  ) ORDER BY V.idVenta desc";
+		$sql = "SELECT V.idVenta, concat(E.nomPila, ' ', E.apPaterno, ' ', E.apMaterno) AS 'nombreEmpleado', V.fecha, V.montoTotal, V.descuentoActual, V.status, V.pagoTarjeta FROM venta V join empleado E on E.idEmpleado=V.idEmpleado WHERE V.idEmpleado='$idEmpleado' and ( DATE_FORMAT(current_timestamp, '%m %d %Y') = DATE_FORMAT(V.fecha, '%m %d %Y')  ) ORDER BY V.idVenta desc";
 		return ejecutarConsulta($sql);
 	}
 
@@ -61,7 +61,7 @@ Class Venta{
 	public function prodEmpleadoEnSucursal($idSucursal, $fechaIni, $fechaFin){
 		$fechaIni = $fechaIni . " 00:00:00";
 		$fechaFin = $fechaFin . " 23:59:59";
-		$sql = "SELECT count(V.idVenta) AS ventasRealizadas, concat(E.nomPila,' ',E.apPaterno,' ',E.apMaterno) as nombre, sum(V.montoTotal) as totalVendido FROM venta V JOIN empleado E ON V.idEmpleado = E.idEmpleado WHERE (E.idSucursal = '$idSucursal') AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY V.idEmpleado ORDER BY totalVendido desc";
+		$sql = "SELECT count(V.idVenta) AS ventasRealizadas, concat(E.nomPila,' ',E.apPaterno,' ',E.apMaterno) AS nombre, sum(V.montoTotal) AS totalVendido FROM venta V JOIN empleado E ON V.idEmpleado = E.idEmpleado WHERE (E.idSucursal = '$idSucursal') AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY V.idEmpleado ORDER BY totalVendido desc";
 		return ejecutarConsulta($sql);
 	}
 
@@ -69,7 +69,7 @@ Class Venta{
 	public function prodEmpleadoEnFranquicia($idFranquicia, $fechaIni, $fechaFin){
 		$fechaIni = $fechaIni . " 00:00:00";
 		$fechaFin = $fechaFin . " 23:59:59";
-		$sql = "SELECT count(V.idVenta) AS ventasRealizadas, concat(E.nomPila,' ',E.apPaterno,' ',E.apMaterno) as nombre, sum(V.montoTotal) as totalVendido, S.nombre as sucursal FROM venta V JOIN empleado E JOIN sucursal S ON (V.idEmpleado = E.idEmpleado) and (E.idSucursal = S.idSucursal) WHERE (S.idFranquicia = '$idFranquicia') AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY V.idEmpleado ORDER BY totalVendido desc";
+		$sql = "SELECT count(V.idVenta) AS ventasRealizadas, concat(E.nomPila,' ',E.apPaterno,' ',E.apMaterno) AS nombre, sum(V.montoTotal) AS totalVendido, S.nombre AS sucursal FROM venta V JOIN empleado E JOIN sucursal S ON (V.idEmpleado = E.idEmpleado) and (E.idSucursal = S.idSucursal) WHERE (S.idFranquicia = '$idFranquicia') AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY V.idEmpleado ORDER BY totalVendido desc";
 		return ejecutarConsulta($sql);
 	}
 
@@ -77,7 +77,7 @@ Class Venta{
 	public function prodEmpleadoGeneral($fechaIni, $fechaFin){
 		$fechaIni = $fechaIni . " 00:00:00";
 		$fechaFin = $fechaFin . " 23:59:59";
-		$sql = "SELECT count(V.idVenta) AS ventasRealizadas, concat(E.nomPila,' ',E.apPaterno,' ',E.apMaterno) as nombre, sum(V.montoTotal) as totalVendido FROM venta V JOIN empleado E ON V.idEmpleado = E.idEmpleado WHERE V.fecha BETWEEN '$fechaIni' AND '$fechaFin' GROUP BY E.idEmpleado ORDER BY totalVendido desc";
+		$sql = "SELECT count(V.idVenta) AS ventasRealizadas, concat(E.nomPila,' ',E.apPaterno,' ',E.apMaterno) AS nombre, sum(V.montoTotal) AS totalVendido FROM venta V JOIN empleado E ON V.idEmpleado = E.idEmpleado WHERE V.fecha BETWEEN '$fechaIni' AND '$fechaFin' GROUP BY E.idEmpleado ORDER BY totalVendido desc";
 		return ejecutarConsulta($sql);
 	}
 
@@ -85,7 +85,7 @@ Class Venta{
 	public function prodSucursalEnFranquicia($idFranquicia, $fechaIni, $fechaFin){
 		$fechaIni = $fechaIni . " 00:00:00";
 		$fechaFin = $fechaFin . " 23:59:59";
-		$sql = "SELECT S.idSucursal, S.nombre as nombreSucursal, count(V.idVenta) AS ventasRealizadas, sum(V.montoTotal) as totalVendido FROM venta V JOIN empleado E RIGHT JOIN sucursal S ON (S.idSucursal=E.idSucursal) AND (V.idEmpleado = E.idEmpleado) WHERE (S.idFranquicia = '$idFranquicia') AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY S.idSucursal ORDER BY totalVendido desc";
+		$sql = "SELECT S.idSucursal, S.nombre AS nombreSucursal, count(V.idVenta) AS ventasRealizadas, sum(V.montoTotal) AS totalVendido FROM venta V JOIN empleado E RIGHT JOIN sucursal S ON (S.idSucursal=E.idSucursal) AND (V.idEmpleado = E.idEmpleado) WHERE (S.idFranquicia = '$idFranquicia') AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY S.idSucursal ORDER BY totalVendido desc";
 		return ejecutarConsulta($sql);
 	}
 
@@ -93,40 +93,40 @@ Class Venta{
 	public function prodFranquiciaGeneral($fechaIni, $fechaFin){
 		$fechaIni = $fechaIni . " 00:00:00";
 		$fechaFin = $fechaFin . " 23:59:59";
-		$sql = "SELECT F.idFranquicia, F.nombre as nombreFranquicia, count(V.idVenta) AS ventasRealizadas, sum(V.montoTotal) as totalVendido FROM (venta V JOIN empleado E JOIN sucursal S) RIGHT JOIN franquicia F ON (S.idFranquicia = F.idFranquicia) AND (S.idSucursal=E.idSucursal) AND (V.idEmpleado = E.idEmpleado) AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY F.idFranquicia ORDER BY totalVendido desc";
+		$sql = "SELECT F.idFranquicia, F.nombre AS nombreFranquicia, count(V.idVenta) AS ventasRealizadas, sum(V.montoTotal) AS totalVendido FROM (venta V JOIN empleado E JOIN sucursal S) RIGHT JOIN franquicia F ON (S.idFranquicia = F.idFranquicia) AND (S.idSucursal=E.idSucursal) AND (V.idEmpleado = E.idEmpleado) AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY F.idFranquicia ORDER BY totalVendido desc";
 		return ejecutarConsulta($sql);
 	}
 
 	public function productosMasVendidos($idFranquicia, $fechaIni, $fechaFin){
 		$fechaIni = $fechaIni . " 00:00:00";
 		$fechaFin = $fechaFin . " 23:59:59";
-		$sql = "SELECT P.nombre, LP.totalVendido FROM (SELECT PV.idProducto, SUM(PV.cantidad) as totalVendido FROM productoVendido PV JOIN sucursal S JOIN venta V ON (PV.idSucursal = S.idSucursal) AND (PV.idVenta = V.idVenta)  WHERE (S.idFranquicia = '$idFranquicia') AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY PV.idProducto) LP JOIN producto P ON LP.idProducto = P.idProducto ORDER BY totalVendido desc";
+		$sql = "SELECT P.nombre, LP.totalVendido FROM (SELECT PV.idProducto, SUM(PV.cantidad) AS totalVendido FROM productoVendido PV JOIN sucursal S JOIN venta V ON (PV.idSucursal = S.idSucursal) AND (PV.idVenta = V.idVenta)  WHERE (S.idFranquicia = '$idFranquicia') AND (V.fecha BETWEEN '$fechaIni' AND '$fechaFin') GROUP BY PV.idProducto) LP JOIN producto P ON LP.idProducto = P.idProducto ORDER BY totalVendido desc";
 		return ejecutarConsulta($sql);
 	}
 
 	public function prodUltimosMeses($cantidadMeses){
-		$sql = "SELECT F.nombre AS franquicia, SUM(V.montoTotal) as totalVendido FROM venta V JOIN empleado E JOIN sucursal S JOIN franquicia F ON (V.idEmpleado = E.idEmpleado) AND (E.idSucursal = S.idSucursal) AND (F.idFranquicia = S.idFranquicia) WHERE V.fecha BETWEEN concat(DATE_SUB(CURDATE(), INTERVAL $cantidadMeses MONTH), ' 00:00:00') AND current_timestamp GROUP BY franquicia ORDER BY totalVendido desc";
+		$sql = "SELECT F.nombre AS franquicia, SUM(V.montoTotal) AS totalVendido FROM venta V JOIN empleado E JOIN sucursal S JOIN franquicia F ON (V.idEmpleado = E.idEmpleado) AND (E.idSucursal = S.idSucursal) AND (F.idFranquicia = S.idFranquicia) WHERE V.fecha BETWEEN concat(DATE_SUB(CURDATE(), INTERVAL $cantidadMeses MONTH), ' 00:00:00') AND current_timestamp GROUP BY franquicia ORDER BY totalVendido desc";
 		return ejecutarConsulta($sql);
 	}
 
 	public function prodUltimosDias($cantidadDias){
-		$sql = "SELECT F.nombre AS franquicia, SUM(V.montoTotal) as totalVendido FROM venta V JOIN empleado E JOIN sucursal S JOIN franquicia F ON (V.idEmpleado = E.idEmpleado) AND (E.idSucursal = S.idSucursal) AND (F.idFranquicia = S.idFranquicia) WHERE V.fecha BETWEEN concat(DATE_SUB(CURDATE(), INTERVAL $cantidadDias DAY), ' 00:00:00') AND current_timestamp GROUP BY franquicia ORDER BY totalVendido desc";
+		$sql = "SELECT F.nombre AS franquicia, SUM(V.montoTotal) AS totalVendido FROM venta V JOIN empleado E JOIN sucursal S JOIN franquicia F ON (V.idEmpleado = E.idEmpleado) AND (E.idSucursal = S.idSucursal) AND (F.idFranquicia = S.idFranquicia) WHERE V.fecha BETWEEN concat(DATE_SUB(CURDATE(), INTERVAL $cantidadDias DAY), ' 00:00:00') AND current_timestamp GROUP BY franquicia ORDER BY totalVendido desc";
 		return ejecutarConsulta($sql);
 	}
 
 	public function topN($cant){
-		$sql = "SELECT concat(E.nomPila, ' ', SUBSTRING(E.apPaterno, 1, 1),'.') AS empleado, SUM(V.montoTotal) as total FROM empleado E JOIN venta V ON V.idEmpleado=E.idEmpleado WHERE V.fecha BETWEEN concat(DATE_SUB(CURDATE(), INTERVAL 1 MONTH), ' 00:00:00') AND current_timestamp GROUP BY empleado ORDER BY total DESC limit 0,$cant";
+		$sql = "SELECT concat(E.nomPila, ' ', SUBSTRING(E.apPaterno, 1, 1),'.') AS empleado, SUM(V.montoTotal) AS total FROM empleado E JOIN venta V ON V.idEmpleado=E.idEmpleado WHERE V.fecha BETWEEN concat(DATE_SUB(CURDATE(), INTERVAL 1 MONTH), ' 00:00:00') AND current_timestamp GROUP BY empleado ORDER BY total DESC limit 0,$cant";
 		return ejecutarConsulta($sql);
 	}
 
 	public function noProductivos($cantidadDias){
 		$sql = "SELECT concat(E.nomPila, ' ', E.apPaterno) AS empleado FROM (
-			SELECT count(V.idVenta) as cantidadVentas, E1.idEmpleado FROM empleado E1 JOIN venta V ON V.idEmpleado=E1.idEmpleado WHERE V.fecha BETWEEN concat(DATE_SUB(CURDATE(), INTERVAL $cantidadDias DAY), ' 00:00:00') AND current_timestamp GROUP BY idEmpleado
+			SELECT count(V.idVenta) AS cantidadVentas, E1.idEmpleado FROM empleado E1 JOIN venta V ON V.idEmpleado=E1.idEmpleado WHERE V.fecha BETWEEN concat(DATE_SUB(CURDATE(), INTERVAL $cantidadDias DAY), ' 00:00:00') AND current_timestamp GROUP BY idEmpleado
 		) ET RIGHT JOIN 
 		(
-			SELECT E2.idEmpleado, E2.nomPila, E2.apPaterno FROM empleado E2 JOIN empleadoPermiso EP ON E2.idEmpleado = EP.idEmpleado WHERE EP.idPermiso = 5 
+			SELECT E2.idEmpleado, E2.nomPila, E2.apPaterno FROM empleado E2 JOIN empleadoPermiso EP ON E2.idEmpleado = EP.idEmpleado WHERE (EP.idPermiso = 5) AND (E2.isActive = 1)
 		) E 
-		ON ET.idEmpleado = E.idEmpleado WHERE ET.idEmpleado is null";
+		ON ET.idEmpleado = E.idEmpleado WHERE ET.idEmpleado IS NULL LIMIT 0,2";
 		return ejecutarConsulta($sql);
 	}
 
